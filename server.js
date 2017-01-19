8b0e4d4baf7ffec53def39fe677d11523afd4fb1
admin-jgaren <admin-jgaren>
2017-01-19 21:16:09 +0100

    redirect

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-
 app.get('*', (req, res) => {
     res.status(200).sendFile(path.join(__dirname + '/dist/de-DE/index.html'))
 });
+
+app.use(express.static(__dirname + '/dist/de-DE'));
+
+
 
 app.listen(process.env.PORT || 8080);
