1b08a4922d503d24f08c11c859a1255f5fc9e7a2
admin-jgaren <admin-jgaren>
2017-01-19 21:26:20 +0100

    redirect

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 
-router.use('/', express.static('dist/de-DE', { redirect: false}));
+router.use('/', express.static('dist', { redirect: false}));
 
 router.get('*', function (req, res, next) {
     res.sendFile(path.resolve('dist/de-DE/index.html'))
