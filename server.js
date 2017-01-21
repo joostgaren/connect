2754ed49f9e57444e55d1af2afac3cf44d6b365b
admin-jgaren <admin-jgaren>
2017-01-21 14:30:32 +0100

    redirect

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/de-DE'));
 
 app.get('*', (req, res) => {
     res.status(200).sendFile(path.join(__dirname + '/dist/de-DE/index.html'))
