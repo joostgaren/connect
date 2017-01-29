b758feedf66641a1be933f42b3b8ce843156a526
admin-jgaren <admin-jgaren>
2017-01-29 16:50:56 +0100

    test

--- src/app/modules/chart/chart.component.ts
+++ src/app/modules/chart/chart.component.ts
@@ -37,46 +37,6 @@ export class ChartComponent implements OnInit, OnChanges {
 
 
 
-  createChart() {
-    let element = this.chartContainer.nativeElement;
-    var barPadding = 10;
-    var barSpacing = this.height / this.data.length;
-    var barHeight = barSpacing - barPadding;
-    var maxValue = d3.max(this.data);
-    var widthScale = this.width / maxValue;
-
-    let svg = d3.select(element).append('svg')
-      .attr('width', this.width)
-      .attr('height', this.height);
-
-    // chart plot area
-    this.chart = svg.append('g')
-      .selectAll('rect')
-      .data(this.data)
-      .enter()
-      .append('rect')
-      .attr('y', function (d, i) { return i * barSpacing })
-      .attr('height', barHeight)
-      .attr('x', 0)
-      .attr('width', function (d) { return d * widthScale })
-      .style('fill', 'steelblue');
-
-  }
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
